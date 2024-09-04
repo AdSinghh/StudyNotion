1d37eb6c2d78820815d67557efa0bec272ec69ca
AdSinghh <132607841+AdSinghh@users.noreply.github.com>
2024-09-04 23:19:39 +0530

    Update razorpay.js

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,7 +1,7 @@
-const Razorpay = require("razorpay");
+const Razorpay = require("razorpay"); // Import the Razorpay library to interact with Razorpay's payment gateway
 
-
+// Create and export an instance of Razorpay with API keys from environment variables
 exports.instance = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY,
-    key_secret: process.env.RAZORPAY_SECRET,
+    key_id: process.env.RAZORPAY_KEY, // The public key ID for Razorpay API, loaded from environment variables
+    key_secret: process.env.RAZORPAY_SECRET, // The private key secret for Razorpay API, also loaded from environment variables
 });
