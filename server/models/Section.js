01cfdbf3c10b68d02972758d2e012aa64b1e9ab8
AdSinghh <132607841+AdSinghh@users.noreply.github.com>
2024-09-04 23:27:11 +0530

    Update Section.js

--- server/models/Section.js
+++ server/models/Section.js
@@ -1,18 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // Import Mongoose to interact with MongoDB
 
 // Define the Section schema
 const sectionSchema = new mongoose.Schema({
+	// Field for storing the name of the section
 	sectionName: {
-		type: String,
+		type: String, // Section name is stored as a string
 	},
+
+	// Field for storing references to sub-sections within the section
 	subSection: [
 		{
-			type: mongoose.Schema.Types.ObjectId,
-			required: true,
-			ref: "SubSection",
+			type: mongoose.Schema.Types.ObjectId, // Type is ObjectId to reference sub-section documents
+			required: true, // This field is mandatory
+			ref: "SubSection", // Reference to the "SubSection" model
 		},
 	],
 });
 
 // Export the Section model
-module.exports = mongoose.model("Section", sectionSchema);
+module.exports = mongoose.model("Section", sectionSchema); // Create and export a Mongoose model named "Section" using the schema
