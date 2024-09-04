8c60d6d5138d52d5f544b0f52ee55a407e50cca1
AdSinghh <132607841+AdSinghh@users.noreply.github.com>
2024-09-04 23:22:50 +0530

    Update CourseProgress.js

--- server/models/CourseProgress.js
+++ server/models/CourseProgress.js
@@ -1,20 +1,22 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose"); // Import Mongoose to interact with MongoDB
 
+// Define the courseProgress schema
 const courseProgress = new mongoose.Schema({
   courseID: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Course",
+    type: mongoose.Schema.Types.ObjectId, // Data type is ObjectId for referencing another document
+    ref: "Course", // References the "Course" model
   },
   userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
+    type: mongoose.Schema.Types.ObjectId, // Data type is ObjectId for referencing another document
+    ref: "user", // References the "user" model
   },
   completedVideos: [
     {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "SubSection",
+      type: mongoose.Schema.Types.ObjectId, // Array of ObjectId references
+      ref: "SubSection", // References the "SubSection" model
     },
   ],
-})
+});
 
-module.exports = mongoose.model("courseProgress", courseProgress)
+// Export the courseProgress model
+module.exports = mongoose.model("courseProgress", courseProgress); // Create and export the "courseProgress" model based on the schema
