18e1e8d6e05859f781ad965d783f950c0ad607f9
AdSinghh <132607841+AdSinghh@users.noreply.github.com>
2024-09-04 23:20:33 +0530

    Update Category.js

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,19 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); // Import the Mongoose library to interact with MongoDB
 
 // Define the Tags schema
 const categorySchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	description: { type: String },
-	courses: [
-		{
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "Course",
-		},
-	],
+    name: {
+        type: String, // The 'name' field is of type String
+        required: true, // This field is required
+    },
+    description: { 
+        type: String // The 'description' field is optional and of type String
+    },
+    courses: [
+        {
+            type: mongoose.Schema.Types.ObjectId, // Each item in the array is an ObjectId type
+            ref: "Course", // Refers to the "Course" model, establishing a relationship
+        },
+    ],
 });
 
 // Export the Tags model
-module.exports = mongoose.model("Category", categorySchema);
+module.exports = mongoose.model("Category", categorySchema); // Create and export the "Category" model based on the defined schema
